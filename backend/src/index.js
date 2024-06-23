aa09d4ee883d95e97dc8a780b611eb32b9bf3480
Muhammad Umair <umair42.dev@gmail.com>
2024-06-24 03:32:34 +0500

    commited

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,11 @@ import { app } from './app.js';
 import { connectDB } from './db/index.js';
 
 dotenv.config();
+
 connectDB()
   .then(() => {
-    app.listen(3001, () => {
-      console.log('Server is running on port 3001');
+    app.listen(3002, () => {
+      console.log('Server is running on port 3002');
     });
   })
   .catch(error => {
